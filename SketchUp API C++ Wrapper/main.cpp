c8fdfdc0b4d9266430a4fc3c761e257b9a64a225
TommyKaneko <tom.kaneko@gmail.com>
2016-11-06 22:47:15 +0000

    Initial Commit

---
+++ SketchUp API C++ Wrapper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SketchUp API C++ Wrapper
+//
+//  Created by Hidetomo Kaneko on 06/11/2016.
+//  Copyright © 2016 TommyKaneko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
