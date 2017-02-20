2b3643fb8e0d3be585a15baf54dbda449c51bdf0
TommyKaneko <tom.kaneko@gmail.com>
2017-02-20 14:51:01 +0000

    Added License Headers

--- SketchUp API C++ Wrapper/Color.cpp
+++ SketchUp API C++ Wrapper/Color.cpp
@@ -1,9 +1,19 @@
-//
 //  Color.cpp
-//  SUEX_SkinUp
+//  Sketchup C++ Wrapper for C API
+//  Copyright (C) 2016  Hidetomo (Tom) Kaneko
 //
-//  Created by Hidetomo Kaneko on 09/10/2016.
-//  Copyright © 2016 Tom Kaneko. All rights reserved.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #include "Color.hpp"
