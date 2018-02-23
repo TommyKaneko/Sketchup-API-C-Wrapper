218561261489d8644d18fd7c2a96bb5b322237d0
Thomas Thomassen <thomas@thomthom.net>
2018-02-23 22:40:53 +0100

    More Radians tests to correct directory.

--- tests/RadiansTests.cpp
+++ tests/RadiansTests.cpp
@@ -1,21 +1,19 @@
 #include "gtest/gtest.h"
 
-#include <cmath>
-
 #include "SUAPI-CppWrapper/Geometry.hpp"
 
 
 TEST(Radians, InitializeWithinRange)
 {
   CW::Radians radians(1.0);
-  ASSERT_DOUBLE_EQ(1.0, radians);	
+  ASSERT_DOUBLE_EQ(1.0, radians);
 }
 
 TEST(Radians, InitializeRollAroundPositive)
 {
   CW::Radians radians(7.0);
   auto expected = 0.71681469282041377;
-  ASSERT_DOUBLE_EQ(expected, radians);	
+  ASSERT_DOUBLE_EQ(expected, radians);
 }
 
 TEST(Radians, InitializeRollAroundNegative)
@@ -25,6 +23,6 @@ TEST(Radians, InitializeRollAroundNegative)
   // is the same as -0.71681469282041377
   CW::Radians radians(-7.0);
   auto expected = 5.5663706143591725;
-  ASSERT_DOUBLE_EQ(expected, radians);	
+  ASSERT_DOUBLE_EQ(expected, radians);
 }
 
