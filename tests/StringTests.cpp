f3ae8146f2c24902d8033f568f73800294178267
Thomas Thomassen <thomas@thomthom.net>
2018-02-23 11:23:26 +0100

    Avoid returning NULL character from String::std_string. Fixes #37.

---
+++ tests/StringTests.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+
+#include <SketchUpAPI/sketchup.h>
+
+#include <string>
+
+#include "SUAPI-CppWrapper/String.hpp"
+
+
+TEST(String, std_string)
+{
+  std::string expected = "Hello World";
+
+  SUStringRef su_string = SU_INVALID;
+  SU(SUStringCreateFromUTF8(&su_string, expected.c_str()));
+  size_t su_string_size = 0;
+  SU(SUStringGetUTF8Length(su_string, &su_string_size));
+
+  CW::String string(su_string);
+  auto std_string = string.std_string();
+
+  ASSERT_EQ(expected.size(), su_string_size);
+  ASSERT_EQ(expected.size(), string.size());
+  ASSERT_EQ(expected.size(), std_string.size());
+
+  ASSERT_STREQ(expected.c_str(), std_string.c_str());
+}
