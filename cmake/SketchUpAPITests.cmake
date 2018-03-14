411fc82fdd82a29f4c3dcb19ca27638d27a4e6d0
TommyKaneko <tom.kaneko@gmail.com>
2018-03-14 10:55:00 +0000

    Work in progress commit for getting CMake to work with Xcode better

--- cmake/SketchUpAPITests.cmake
+++ cmake/SketchUpAPITests.cmake
@@ -21,9 +21,12 @@ source_group(
 )
 
 # TODO(thomthom): Set up per-platform post-build commands.
-add_custom_command(TARGET SketchUpAPITests POST_BUILD
-  # xcopy seem to require at least one \ slash in the source path, otherwise it
-  # appear to fail.
-  COMMAND xcopy \"${SLAPI_BINARIES_PATH}\\sketchup\\x64\\SketchUpAPI.dll\" $(OutputPath) /D /Y
-  COMMAND xcopy \"${SLAPI_BINARIES_PATH}\\sketchup\\x64\\SketchUpCommonPreferences.dll\" $(OutputPath) /D /Y
-)
+if ( MSVC )
+  add_custom_command(TARGET SketchUpAPITests POST_BUILD
+    # xcopy seem to require at least one \ slash in the source path, otherwise it
+    # appear to fail.
+    COMMAND xcopy \"${SLAPI_BINARIES_PATH}\\sketchup\\x64\\SketchUpAPI.dll\" $(OutputPath) /D /Y
+    COMMAND xcopy \"${SLAPI_BINARIES_PATH}\\sketchup\\x64\\SketchUpCommonPreferences.dll\" $(OutputPath) /D /Y
+  )
+else()
+endif()
